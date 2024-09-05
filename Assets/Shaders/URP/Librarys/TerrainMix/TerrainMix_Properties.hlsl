d09b3d028c2d170b6b014a7aa884c2976d45cfe2
caojian <caojianmanager@163.com>
2024-09-05 11:41:29 +0800

    fix properties

--- Assets/Shaders/URP/Librarys/TerrainMix/TerrainMix_Properties.hlsl
+++ Assets/Shaders/URP/Librarys/TerrainMix/TerrainMix_Properties.hlsl
@@ -19,4 +19,36 @@ float _Layer3_HeightContrast;
 float _Layer4_HeightContrast;
 CBUFFER_END
 
+#ifdef UNITY_DOTS_INSTANCING_ENABLED
+UNITY_DOTS_INSTANCING_START(MaterialPropertyMetadata)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer1_BaseMap_ST)
+    UNITY_DOTS_INSTANCED_PROP(float4, _Layer2_BaseMap_ST)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer3_BaseMap_ST)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer4_BaseMap_ST)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer1_BaseColor)
+    UNITY_DOTS_INSTANCED_PROP(float4, _Layer2_BaseColor)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer3_BaseColor)
+    UNITY_DOTS_INSTANCED_PROP(float4 , _Layer4_BaseColor)
+    UNITY_DOTS_INSTANCED_PROP(float , _BlendContrast)
+    UNITY_DOTS_INSTANCED_PROP(float, _Layer1_HeightContrast)
+    UNITY_DOTS_INSTANCED_PROP(float , _Layer2_HeightContrast)
+    UNITY_DOTS_INSTANCED_PROP(float , _Layer3_HeightContrast)
+    UNITY_DOTS_INSTANCED_PROP(float , _Layer4_HeightContrast)
+UNITY_DOTS_INSTANCING_END(MaterialPropertyMetadata)
+
+#define _Layer1_BaseMap_ST          UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer1_BaseMap_ST)
+#define _Layer2_BaseMap_ST          UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer2_BaseMap_ST)
+#define _Layer3_BaseMap_ST          UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer3_BaseMap_ST)
+#define _Layer4_BaseMap_ST          UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer4_BaseMap_ST)
+#define _Layer1_BaseColor           UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer1_BaseColor)
+#define _Layer2_BaseColor           UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer2_BaseColor)
+#define _Layer3_BaseColor           UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer3_BaseColor)
+#define _Layer4_BaseColor           UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float4  , _Layer4_BaseColor)
+#define _BlendContrast              UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float  , _BlendContrast)
+#define _Layer1_HeightContrast      UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float  , _Layer1_HeightContrast)
+#define _Layer2_HeightContrast      UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float  , _Layer2_HeightContrast)
+#define _Layer3_HeightContrast      UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float  , _Layer3_HeightContrast)
+#define _Layer4_HeightContrast      UNITY_ACCESS_DOTS_INSTANCED_PROP_WITH_DEFAULT(float  , _Layer4_HeightContrast)
+#endif
+
 #endif
